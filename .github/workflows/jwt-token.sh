22006f9e07fdf4b4e210c1c8403a8b19ba6fe920
saravananjobs2017 <57668555+saravananjobs2017@users.noreply.github.com>
2024-11-05 11:47:02 +0530

    Update jwt-token.sh

--- .github/workflows/jwt-token.sh
+++ .github/workflows/jwt-token.sh
@@ -2,13 +2,13 @@
 set -x
 #set -o pipefail
 
-client_id="1045288" # Client ID as first argument
+client_id="1045288"  # Client ID as first argument
 
-pem=$( cat .github/workflows/test-poc-sara.2024-11-03.private-key.pem ) # file path of the private key as second argument
+pem=$(cat .github/workflows/test-poc-sara.2024-11-03.private-key.pem)  # File path of the private key
 
 now=$(date +%s)
-iat=$((${now} - 60)) # Issues 60 seconds in the past
-exp=$((${now} + 600)) # Expires 10 minutes in the future
+iat=$((${now} - 60))  # Issues 60 seconds in the past
+exp=$((${now} + 600))  # Expires 10 minutes in the future
 
 b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
 
@@ -17,7 +17,7 @@ header_json='{
     "alg":"RS256"
 }'
 # Header encode
-header=$( echo -n "${header_json}" | b64enc )
+header=$(echo -n "${header_json}" | b64enc)
 
 payload_json="{
     \"iat\":${iat},
@@ -25,15 +25,23 @@ payload_json="{
     \"iss\":\"${client_id}\"
 }"
 # Payload encode
-payload=$( echo -n "${payload_json}" | b64enc )
+payload=$(echo -n "${payload_json}" | b64enc)
 
 # Signature
-header_payload="${header}"."${payload}"
+header_payload="${header}.${payload}"
+
+# Save the private key to a temporary file
+private_key_file=$(mktemp)
+echo -n "${pem}" > "${private_key_file}"
+
 signature=$(
-    openssl dgst -sha256 -sign <(echo -n "${pem}") \
+    openssl dgst -sha256 -sign "${private_key_file}" \
     <(echo -n "${header_payload}") | b64enc
 )
 
+# Clean up the temporary file
+rm -f "${private_key_file}"
+
 # Create JWT
-JWT="${header_payload}"."${signature}"
+JWT="${header_payload}.${signature}"
 printf '%s\n' "JWT: $JWT"
